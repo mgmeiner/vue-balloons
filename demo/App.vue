738b5b7e235d0d4f23b7a2c10ebb4b61af7b1f22
mgmeiner <gmeiner.m@gmail.com>
2017-01-24 22:15:10 +0100

    update demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,47 +1,47 @@
 <template>
   <div class="demo-container">
-    <span class="small"><surpriseTextGenerator text="SmallBalloons" /></span>
-    <span class="medium"><surpriseTextGenerator text="MediumBalloons" /></span>
-    <span class="large"><surpriseTextGenerator text="LargeBalloons" /></span>
-    <span class="ultra"><surpriseTextGenerator text="UltraLargeBalloons" /></span>
-    <span class="extreme"><surpriseTextGenerator text="ExtremeLargeBalloons" /></span>
+    <div class="small"><vueBalloons text="Small Balloons" /></div>
+    <div class="medium"><vueBalloons text="Medium Balloons" /></div>
+    <div class="large"><vueBalloons text="Large Balloons" /></div>
+    <div class="ultra"><vueBalloons text="Ultra Large Balloons" /></div>
+    <div class="extreme"><vueBalloons text="Extreme Large Balloons" /></div>
   </div>
 </template>
 
 <script>
-  import surpriseTextGenerator from '../lib/SurpriseTextGenerator';
+  import vueBalloons from '../lib/VueBalloons';
 
   export default {
     name: 'app',
     components: {
-      surpriseTextGenerator
+      vueBalloons
     }
   };
 </script>
 
-<style lang="less"> 
+<style lang="less" scoped> 
   .demo-container {   
     margin-top: 10vh;
     margin-left: 10vw;
-  }
 
-  .small {
-    font-size: 1rem;
-  }
+    .small {
+      font-size: 1rem;
+    }
 
-  .medium {
-    font-size: 2rem;
-  }
+    .medium {
+      font-size: 2rem;
+    }
 
-  .large {
-    font-size: 3rem;
-  }
+    .large {
+      font-size: 3rem;
+    }
 
-  .ultra {
-    font-size: 4rem;
-  }
+    .ultra {
+      font-size: 4rem;
+    }
 
-  .extreme {
-    font-size: 10rem;
+    .extreme {
+      font-size: 10rem;
+    }
   }
 </style>
